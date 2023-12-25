bfce37ee2ea88298d3ba3ba3c3dd7e8103b68c32
ChercheLYR <78736619+ChercheLYR@users.noreply.github.com>
2023-12-25 16:45:09 +0000

    zhuti Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/f71d.html","posts/3eeb.html","posts/de09.html","posts/92ae.html","posts/f76e.html","posts/3a64.html"];function toRandomPost(){
+var posts=["posts/f71d.html","posts/de09.html","posts/3eeb.html","posts/92ae.html","posts/f76e.html","posts/3a64.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
