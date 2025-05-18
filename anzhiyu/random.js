10311fb108895fbe47c4f056fa65523672f06cbe
cherchelyr <1210693207@qq.com>
2025-05-18 16:04:53 +0800

    Site updated: 2025-05-18 16:04:53

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["posts/cb7d.html","posts/f71d.html","posts/de09.html","posts/f76e.html","posts/92ae.html","posts/3a64.html"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
