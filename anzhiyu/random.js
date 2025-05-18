c93655954c1967b6122e6c937d733d9271e313a9
ChercheLYR <78736619+ChercheLYR@users.noreply.github.com>
2025-05-18 08:38:47 +0000

    修改了一些格式 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/cb7d.html","posts/f71d.html","posts/de09.html","posts/f76e.html","posts/92ae.html","posts/3a64.html"];function toRandomPost(){
+var posts=["posts/155c.html","posts/cb7d.html","posts/f71d.html","posts/bdb8.html","posts/de09.html","posts/4c06.html","posts/cf1d.html","posts/92ae.html","posts/f76e.html","posts/3a64.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
